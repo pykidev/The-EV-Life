09431b8d9ffaca9d40bdd5fc49ab235c1938d5bf
kldenis2001 <kldenis2001@gmail.com>
2023-07-05 02:47:27 +0000

    Form tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,77 @@
-// ignore_for_file: depend_on_referenced_packages
+// // ignore_for_file: depend_on_referenced_packages
+
+// import 'package:flutter/material.dart';
+// import 'package:location_app_net_ninjas/pages/choose_location.dart';
+// import 'package:location_app_net_ninjas/pages/home.dart';
+// import 'package:location_app_net_ninjas/pages/loading.dart';
+
+// void main() => runApp(MaterialApp(
+//   initialRoute: '/',
+//   routes: {
+//     '/': (context) => Loading(),
+//     '/home': (context) => Home(),
+//     '/location': (context) => ChooseLocation(),
+//   }
+// ));
+
 
 import 'package:flutter/material.dart';
-import 'package:location_app_net_ninjas/pages/choose_location.dart';
-import 'package:location_app_net_ninjas/pages/home.dart';
-import 'package:location_app_net_ninjas/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => Loading(),
-    '/home': (context) => Home(),
-    '/location': (context) => ChooseLocation(),
-  }
+  home: Scaffold(
+    appBar: AppBar(
+      title: const Text('Flutter Form'),
+      centerTitle: true,
+    ) ,
+    body: const MyCustomForm()
+    ,)
 ));
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() => MyCustomFormState();
+}
+
+class MyCustomFormState extends State<MyCustomForm>{
+  // Define a global key that uniquely identifies the form widget
+  //and allows validation of the form
+  //Note: This is a 'GlobalKey<FormState>'
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context){
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          //Add TextFormFields and ElevatedButton here
+          TextFormField(
+            //the validator receives the text that the user has entered
+            validator: (value) {
+              if(value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              //Validate returns true if form is valid, or false otherwise
+              if(_formKey.currentState!.validate()){
+                //if the form is valid, display snackbar.
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Processing Data')),
+                );
+              }
+            }, 
+            child: Text('Submit')
+            ),
+
+
+        ],
+      ),
+    );
+  }
+}
